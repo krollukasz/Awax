024abe3c811e3c2bfc271385fc141ae6be19e7d9
Łukasz Król <lionel@o2.pl>
2019-12-17 13:51:44 +0100

    Add a simple button to scroll up

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Solution based on example from Stack Overflow
+// When the user scrolls down 130px from the top of the document, show the button
+
+window.onscroll = function() {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 130 || document.documentElement.scrollTop > 130) {
+    document.getElementById("goToTop").style.display = "block";
+  } else {
+    document.getElementById("goToTop").style.display = "none";
+  };
+};
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  $("html, body").animate({scrollTop:0}, "slow");
+};
